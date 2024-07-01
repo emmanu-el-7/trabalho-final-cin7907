8af5312594614656cae20d3069d6e165c0bfea41
Emmanuel Rodrigues da Rocha Costa <rodriguesemanuel021@gmail.com>
2024-07-01 14:24:48 -0300

    classes cliente e ordemserviço

---
+++ Cliente.py
@@ -0,0 +1,6 @@
+class Cliente:
+    def __init__(self, nome, contato, cpf, veiculos):
+        self.nome = nome
+        self.contato = contato
+        self.cpf = cpf
+        self.veiculos = veiculos
